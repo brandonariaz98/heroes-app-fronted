669f31bc596cb3a33bbe40391b1df4abec57d3b4
Brandon Arias <2017030706@upsin.edu.mx>
2026-04-16 16:39:11 -0700

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,11 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
